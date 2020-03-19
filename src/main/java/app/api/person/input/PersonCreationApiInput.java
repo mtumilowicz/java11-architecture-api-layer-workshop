cad7c94c84a4a3c5bf1158cef2e5a707f58298f1
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-19 11:45:50 +0100

    PersonCreationApiInput

--- src/main/java/app/api/person/input/PersonCreationApiInput.java
+++ src/main/java/app/api/person/input/PersonCreationApiInput.java
@@ -1,14 +1,30 @@
 package app.api.person.input;
 
+import app.domain.ErrorCode;
+import app.domain.Failure;
 import app.domain.person.PersonCreationInput;
+import io.vavr.control.Either;
+import io.vavr.control.Option;
+import io.vavr.control.Validation;
+
+import java.util.Objects;
 
 public class PersonCreationApiInput {
 
     String name;
 
-    public PersonCreationInput toDomain() {
-        return PersonCreationInput.builder()
-                .name(name)
-                .build();
+    public Either<Failure, PersonCreationInput> toDomain() {
+        return isNotNull(name, "name")
+                .map(validName -> PersonCreationInput.builder()
+                        .name(validName)
+                        .build())
+                .toEither();
+    }
+
+    static <T> Validation<Failure, T> isNotNull(T obj, String name) {
+        return Option.of(obj)
+                .filter(Objects::nonNull)
+                .map(Validation::<Failure, T>valid)
+                .getOrElse(Validation.invalid(Failure.fromUserError(ErrorCode.VALIDATION_ERROR, name, "Cannot be null")));
     }
 }
