a0672b5b60deb81e84b097d58e7cec2341fe4141
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-20 13:02:50 +0100

    PersonController change root url

--- src/main/java/app/gateway/person/PersonController.java
+++ src/main/java/app/gateway/person/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@RequestMapping("persons")
+@RequestMapping("app/persons")
 public class PersonController {
 
     @Autowired
