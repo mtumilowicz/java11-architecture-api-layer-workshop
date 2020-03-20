8a9094dc11475242a16d8fa84d85495fb4025bbc
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-20 18:13:29 +0100

    Application.java

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Java11MockmvcControllerLayerArchitectureWorkshopApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Java11MockmvcControllerLayerArchitectureWorkshopApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
