1b271c00b95a08afea8aea9dbe0af3667414e1bc
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-03-22 22:32:18 +0100

    ApiOutput as a class with statuses

--- src/main/java/app/gateway/answers/output/ApiOutput.java
+++ src/main/java/app/gateway/answers/output/ApiOutput.java
@@ -2,19 +2,29 @@ package app.gateway.answers.output;
 
 import app.domain.results.AppError;
 import app.domain.results.UserError;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public interface ApiOutput {
+@Builder(access = AccessLevel.PACKAGE)
+@Getter
+public class ApiOutput {
+
+    private String status;
+
+    private Map<String, Object> data;
 
     static ApiOutput error(List<AppError> appErrors) {
         var apiErrors = appErrors.stream()
                 .map(AppError::asMap)
                 .collect(Collectors.toList());
 
-        return ErrorApiOutput.builder()
+        return ApiOutput.builder()
+                .status("error")
                 .data(Map.of("errors", apiErrors))
                 .build();
     }
@@ -24,13 +34,15 @@ public interface ApiOutput {
                 .map(UserError::asMap)
                 .collect(Collectors.toList());
 
-        return FailApiOutput.builder()
+        return ApiOutput.builder()
+                .status("fail")
                 .data(Map.of("errors", apiErrors))
                 .build();
     }
 
     static ApiOutput success(String name, Object data) {
-        return SuccessApiOutput.builder()
+        return ApiOutput.builder()
+                .status("success")
                 .data(Map.of(name, data))
                 .build();
     }
